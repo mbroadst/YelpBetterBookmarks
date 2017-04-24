3975866ebafdb15abe584e8bf494e7f64cc9dd5e
Matt Broadstone <mbroadst@gmail.com>
2017-04-24 17:01:37 -0400

    feat(drawer): integrate basic filter view into main app in drawer

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ import {
   View
 } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import Drawer from 'react-native-drawer';
+import FilterView from './components/filter-view';
+import Button from './components/button';
 
 const { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
@@ -40,25 +43,53 @@ export default class App extends Component {
     this.onRegionChange = this.onRegionChange.bind(this);
   }
 
+  openDrawer(){
+    this.drawer.open()
+  }
+
   render() {
+    const filterView =
+      <FilterView
+        closeDrawer={ () => this.drawer.close() }
+      />
+
     return (
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          style={styles.map}
-          region={this.state.region}
-          onRegionChange={this.onRegionChange}
-        >
-          {this.state.markers.map((marker, idx) => (
-            <MapView.Marker
-              key={idx}
-              coordinate={marker.coordinate}
-              title={marker.title}
-              description={marker.description}
-            />
-          ))}
-        </MapView>
-      </View>
+      <Drawer
+        type='overlay'
+        ref={ (ref) => this.drawer = ref }
+        content={ filterView }
+        tapToClose={ true }
+        openDrawerOffset={ 0.2 } // 20% gap on the right side of drawer
+        panCloseMask={ 0.2 }
+        closedDrawerOffset={ -3 }
+        styles={ drawerStyles }
+        tweenHandler={ (ratio) => ({
+          main: { opacity: (2 - ratio) / 2 }
+        }) }
+      >
+        <View style={styles.container}>
+          <MapView
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}
+            region={this.state.region}
+            onRegionChange={this.onRegionChange}
+          >
+            {this.state.markers.map((marker, idx) => (
+              <MapView.Marker
+                key={idx}
+                coordinate={marker.coordinate}
+                title={marker.title}
+                description={marker.description}
+              />
+            ))}
+          </MapView>
+
+          <Button
+            onPress={ () => this.openDrawer() }
+            text="Open Drawer"
+          />
+        </View>
+      </Drawer>
     );
   }
 
@@ -69,6 +100,11 @@ export default class App extends Component {
 
 App.propTypes = {
   provider: MapView.ProviderPropType
+};
+
+const drawerStyles = {
+  drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
+  main: { paddingLeft: 3 },
 };
 
 const styles = StyleSheet.create({
